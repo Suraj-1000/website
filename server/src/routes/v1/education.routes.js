57436eed18f0f53680d1749d2fc02965c3183faa
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-08 18:53:11 +0545

    updated

--- server/src/routes/v1/education.routes.js
+++ server/src/routes/v1/education.routes.js
@@ -3,14 +3,15 @@ const educationController = require('@/controllers/education.controller');
 const { protect, authorize } = require('@/middlewares/auth.middleware');
 
 const router = Router();
+const adminOnly = [protect, authorize('admin')];
 
 router.route('/')
    .get(educationController.getAll)
-   .post(protect, authorize('admin'), educationController.create);
+   .post(adminOnly, educationController.create);
 
 router.route('/:id')
-   .put(protect, authorize('admin'), educationController.update)
-   .patch(protect, authorize('admin'), educationController.update)
-   .delete(protect, authorize('admin'), educationController.delete);
+   .put(adminOnly, educationController.update)
+   .patch(adminOnly, educationController.update)
+   .delete(adminOnly, educationController.delete);
 
 module.exports = router;
--- server/src/routes/v1/experience.routes.js
+++ server/src/routes/v1/experience.routes.js
@@ -3,14 +3,15 @@ const experienceController = require('@/controllers/experience.controller');
 const { protect, authorize } = require('@/middlewares/auth.middleware');
 
 const router = Router();
+const adminOnly = [protect, authorize('admin')];
 
 router.route('/')
    .get(experienceController.getAll)
-   .post(protect, authorize('admin'), experienceController.create);
+   .post(adminOnly, experienceController.create);
 
 router.route('/:id')
-   .put(protect, authorize('admin'), experienceController.update)
-   .patch(protect, authorize('admin'), experienceController.update)
-   .delete(protect, authorize('admin'), experienceController.delete);
+   .put(adminOnly, experienceController.update)
+   .patch(adminOnly, experienceController.update)
+   .delete(adminOnly, experienceController.delete);
 
 module.exports = router;
