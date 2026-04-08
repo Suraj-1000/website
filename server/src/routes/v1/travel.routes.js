b5df24bfde8ca58bef031fa41bbd07cfd27bd043
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-08 18:53:40 +0545

    updated

--- server/src/routes/v1/travel.routes.js
+++ server/src/routes/v1/travel.routes.js
@@ -3,9 +3,15 @@ const travelController = require('@/controllers/travel.controller');
 const { protect, authorize } = require('@/middlewares/auth.middleware');
 
 const router = Router();
+const adminOnly = [protect, authorize('admin')];
 
 router.route('/')
    .get(travelController.getAll)
-   .post(protect, authorize('admin'), travelController.create);
+   .post(adminOnly, travelController.create);
+
+router.route('/:id')
+   .put(adminOnly, travelController.update)
+   .patch(adminOnly, travelController.update)
+   .delete(adminOnly, travelController.delete);
 
 module.exports = router;
