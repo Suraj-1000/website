6ae24c51564f138347add195567326c7f7b80037
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-08 18:53:34 +0545

    updated

--- server/src/routes/v1/reference.routes.js
+++ server/src/routes/v1/reference.routes.js
@@ -3,14 +3,15 @@ const referenceController = require('@/controllers/reference.controller');
 const { protect, authorize } = require('@/middlewares/auth.middleware');
 
 const router = Router();
+const adminOnly = [protect, authorize('admin')];
 
 router.route('/')
    .get(referenceController.getAll)
-   .post(protect, authorize('admin'), referenceController.create);
+   .post(adminOnly, referenceController.create);
 
 router.route('/:id')
-   .put(protect, authorize('admin'), referenceController.update)
-   .patch(protect, authorize('admin'), referenceController.update)
-   .delete(protect, authorize('admin'), referenceController.delete);
+   .put(adminOnly, referenceController.update)
+   .patch(adminOnly, referenceController.update)
+   .delete(adminOnly, referenceController.delete);
 
 module.exports = router;
