e4403e4be1a4aa993a81b8c54bf71b70de77eca6
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-10 23:48:58 +0545

    updated

--- server/src/controllers/travel.controller.js
+++ server/src/controllers/travel.controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('@/middlewares/asyncHandler');
 const travelService = require('@/services/travel.service');
+const { NotFoundException } = require('@/exceptions/error.exception');
 
 class TravelController {
    getAll = asyncHandler(async (req, res) => {
@@ -10,6 +11,18 @@ class TravelController {
    create = asyncHandler(async (req, res) => {
       const travel = await travelService.create(req.body);
       res.status(201).json({ success: true, data: travel });
+   });
+
+   update = asyncHandler(async (req, res) => {
+      const travel = await travelService.update(req.params.id, req.body);
+      if (!travel) throw new NotFoundException('Travel not found');
+      res.status(200).json({ success: true, data: travel });
+   });
+
+   delete = asyncHandler(async (req, res) => {
+      const result = await travelService.delete(req.params.id);
+      if (!result) throw new NotFoundException('Travel not found');
+      res.status(200).json({ success: true, data: {} });
    });
 }
 
