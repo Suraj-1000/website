91e1767f1f88066429cc61a5cb6c454c9edae0ba
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-15 23:29:53 +0545

    Step 7: Update main.tsx with routing setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/assets/styles/global.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "@/routes/route.jsx";
 
-import App from './App.jsx'
+const router = createBrowserRouter(routes);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
+
+createRoot(rootElement).render(
+   <StrictMode>
+      <RouterProvider router={router} />
+   </StrictMode>
+);
