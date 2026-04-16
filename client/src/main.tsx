77f41087c51ced4735c350c9cfe4484a76d41e08
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-16 23:14:07 +0545

    feat: complete main.tsx bootstrap with router and auth provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,5 +3,17 @@ import { createRoot } from "react-dom/client";
 import "@/assets/styles/global.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "@/routes/route.jsx";
+import { AuthProvider } from "@/services/context/AuthContext";
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
+
 
 
