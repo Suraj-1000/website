b99639e36776179060cd6d04f7bc586eeb2f4c2e
Suraj Kunwor <dsurajkunwor101@gmail.com>
2026-04-22 21:18:15 +0545

    refactor(theme): optimize ThemeSwitcher UI responsiveness

--- client/src/components/theme/ThemeSwitcher.jsx
+++ client/src/components/theme/ThemeSwitcher.jsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 
 export function ThemeSwitcher() {
-   const { theme, setTheme } = useTheme();
+   const [mounted, setMounted] = useState(false);
+   const { setTheme, resolvedTheme } = useTheme();
+
+   useEffect(() => {
+      setMounted(true);
+   }, []);
+
+   if (!mounted) {
+      return null;
+   }
 
    return (
       <Button
          variant="outline"
          size="icon"
          className="rounded-full border cursor-pointer"
-         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+         onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
       >
          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
